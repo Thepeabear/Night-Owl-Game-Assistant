881520dd4c848d2cfc8c580a349f1effdeabf6b4
Jeremiah Pope <thepeabear@gmail.com>
2023-04-24 19:08:51 -0500

    fixed alot of things

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 const apiKey1 = "will be used";
 const baseURL1 = "will be used";
+const btnEl = document.querySelector(".btn");
+const searchForm = document.getElementById("input-form");
+const recommendations = document.getElementById("recommended-games");
 var getReviews = function (reviews) {
   const options = {
     method: "GET",
@@ -17,53 +20,71 @@ var getReviews = function (reviews) {
     .then((response) => console.log(response))
     .catch((err) => console.error(err));
 };
+const searchBar = document.getElementById("search-form");
+let searchTerm = "";
+searchForm.addEventListener("submit", (event) => {
+  console.log("submit");
+  event.preventDefault();
+  let inputEl = document.getElementById("input");
+  console.log(inputEl.value);
+  getSearchResults(inputEl);
+});
 const apiKey2 = "f0aa44ff4b9b8f5166e12cadfd8d1658e871d572";
 const baseURL2 = "https://www.giantbomb.com/api/";
 const $ = jQuery;
 
-// Function to get game recommendations based on platform and genre
-function getRecommendations(platform, genre) {
-  // Make the API call
-  $.ajax({
-    type: "GET",
-    url:
-      baseURL2 +
-      "games/?api_key=" +
-      apiKey2 +
-      "&format=json&field_list=name,image&filter=platforms:" +
-      platform +
-      ",genres:" +
-      genre +
-      "&sort=original_release_date:desc&limit=10",
-    dataType: "jsonp",
-    crossDomain: true,
-    success: function (data) {
+function getSearchResults(genre) {
+  // Construct the API URL
+  const apiUrl = `${baseURL2}search/?api_key=${apiKey2}&format=json&query="${genre}"&resources=game`;
+  // Make the fetch request
+  const options = {
+    method: "GET",
+  };
+  fetch(apiUrl, options)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
       // Get the results array from the API response
       const results = data.results;
       // Clear any existing recommendations from the page
-      $("#recommended-games").empty();
-      // Iterate through the results and append the game names and images to the recommended-games div
-      for (let i = 0; i < results.length; i++) {
-        $("#recommended-games").append(
-          "<div class='game'><h3>" +
-            results[i].name +
-            "</h3><img src='" +
-            results[i].image.small_url +
-            "'></div>"
-        );
+      $("#search-results").empty();
+      // Create a new container div for the game list
+      const resultListEl = $("<div>").addClass("game-list");
+      // Create an ordered list for the games
+      const olEl = $("<ol>");
+      const ol2El = $("<ol>");
+      // Iterate through the results and append the game names and images to the ol element
+      for (let i = 0; i < 4; i++) {
+        const gameEl = $("<li>").addClass("game");
+        const nameEl = $("<h2>").text(results[i].name);
+        const imgEl = $("<img>").attr("src", results[i].image.small_url);
+        gameEl.append(nameEl, imgEl);
+        olEl.append(gameEl);
       }
-    },
-    error: function (error) {
+      for (let i = 3; i < 7; i++) {
+        const gameEl = $("<li>").addClass("game");
+        const nameEl = $("<h2>").text(results[i].name);
+        const imgEl = $("<img>").attr("src", results[i].image.small_url);
+        console.log(nameEl);
+        gameEl.append(nameEl, imgEl);
+        ol2El.append(gameEl);
+      }
+      resultListEl.append(olEl);
+      recommendations.append(ol2El);
+
+      // Append the resultListEl to #search-results
+      $("#search-results").append(resultListEl);
+    })
+    .catch((error) => {
       console.log("Error: " + error.message);
-    },
-  });
+    });
 }
 
-// Call the getRecommendations function when the search button is clicked
+// Call the getSearchResults function when the search button is clicked
 $("#searchBtn button").click(function () {
   // Get the selected platform and genre from the dropdown menus
   const platform = $("#filter-select").val();
   const genre = $("#search-form input").val();
-  // Call the getRecommendations function with the selected platform and genre
-  getRecommendations(platform, genre);
+  // Call the getSearchResults function with the selected platform and genre
+  getSearchResults(platform, genre);
 });
